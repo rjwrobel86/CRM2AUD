7e5094aad91925614c9596cc607fa2ea6ef277ae
Robert Wrobel <25327998+rjwrobel86@users.noreply.github.com>
2021-04-10 10:36:08 -0500

    Update auder.py
    Now outputs audiences for google and facebook, and also offline conversions for facebook

--- auder.py
+++ auder.py
@@ -1,59 +1,67 @@
-#accepts sales records as "saleslog.csv"
-#exports records ready to be uploaded to Google Ads and Facebook Ads as custom audiences as "fbadsaud.csv" and "goadsaud.csv"
-#csv must be formatted with first row as column names 
+#!/usr/bin/env python
+# coding: utf-8
+
+#accepts "SoldLogExport.csv"
+#outputs audiences for google and facebook, as well as offline conversions for facebook
 
 import os
 import pandas as pd
 import numpy as np
+import pandas as pd
 
-#import sales records csv file as pandas dataframe
-data = pd.read_csv('saleslog.csv')
+data = pd.read_csv('SoldLogExport.csv')
 
-#select relevant columns
-fbframe = data[['SaleDate', 'FirstName', 'LastName', 'Email', 'EveningPhone', 'DayPhone', 'CellPhone', 'PostalCode', 'State', 'Revenue1', 'Revenue2', 'ProductID', 'SaleID']]
+fbframe = data[['SoldDate', 'FirstName', 'LastName', 'Email', 'EmailAlt', 'EvePhone', 'DayPhone', 'CellPhone', 'PostalCode', 'State', 'FrontGross', 'BackGross', 'SoldNote', 'VehicleVIN', 'DealNumber']]
 
-#add static columns
 fbframe['Country'] = 'US'
 fbframe['Event'] = 'Purchase'
 fbframe['Currency'] = 'USD'
-
-#combine value measures
-fbframe['SaleValue'] = fbframe['Revenue1'] + fbframe['Revenue2']
-
-#create new phone variables with formatting for Facebook
-fbframe['Phone'] = '+1' + data['EveningPhone'].astype(str)
+fbframe['Value'] = fbframe['FrontGross'] + fbframe['BackGross']
+fbframe['Phone'] = '+1' + data['EvePhone'].astype(str)
 fbframe['Phone2'] = '+1' + data['DayPhone'].astype(str)
 fbframe['Phone3'] = '+1' + data['CellPhone'].astype(str)
+fbframe['Email2'] = data['EmailAlt']
 
-#drop values prior phone variables - new variabels created above
 fbframe.drop(['EvePhone'], axis=1)
 fbframe.drop(['DayPhone'], axis=1)
 fbframe.drop(['CellPhone'], axis=1)
 fbframe.drop(['EmailAlt'], axis=1)
 
-#remove unnecessary columns
-fbframe = fbframe[['SaleDate','FirstName', 'LastName', 'Email', 'PostalCode', 'Phone', 'Phone2', 'Phone3', 'Country', 'State', 'Event', 'SaleValue', 'Currency', 'ProductID', 'SaleID']]
-
-#fill in blank or negative valued sales with '1' instead of 0 or less to prevent error / not being counted
-fbframe.loc[(fbframe.SaleValue <= 0),'SaleValue']=1
+fbframe.loc[(fbframe.Value <= 0),'Value']=1
 fbframe.fillna(1, inplace=True)
 
-#drop all columns google doesn't want
 googframe = fbframe.drop(['State'], axis=1)
 googframe = googframe.drop(['Phone2'], axis=1)
 googframe = googframe.drop(['Phone3'], axis=1)
 googframe = googframe.drop(['Email2'], axis=1)
 googframe = googframe.drop(['Event'], axis=1)
-googframe = googframe.drop(['SaleValue'], axis=1)
+googframe = googframe.drop(['Value'], axis=1)
 googframe = googframe.drop(['Currency'], axis=1)
-googframe = googframe.drop(['ProductID'], axis=1)
-googframe = googframe.drop(['SaleID'], axis=1)
+googframe = googframe.drop(['VehicleVIN'], axis=1)
+googframe = googframe.drop(['DealNumber'], axis=1)
+googframe = googframe.drop(['SoldDate'], axis=1)
 
-#convert to string to make Facebook happy
 fbframe['Phone'] = fbframe['Phone'].astype(str)
 fbframe['Phone2'] = fbframe['Phone2'].astype(str)
 fbframe['Phone3'] = fbframe['Phone3'].astype(str)
 
-#export both dataframes as csv files 100% prepared for upload to each ad platform
-fbframe.to_csv('fbsalelog.csv', index=False)
-googframe.to_csv('googsalelog.csv', index=False)
+fbframe['Phone'] = fbframe['Phone'].str.rstrip('.0')
+fbframe['Phone2'] = fbframe['Phone2'].str.rstrip('.0')
+fbframe['Phone3'] = fbframe['Phone3'].str.rstrip('.0')
+
+fbframe2 = fbframe.rename(columns={'SoldDate':'event_time','FirstName':'fn','LastName':'ln','Email':'email','Email2':'email','Phone':'phone','Phone2':'phone','Phone3':'phone','Country':'country','State':'st','City':'ct','Event':'event_name','Value':'value','Currency':'currency','VehicleVIN':'item_number','DealNumber':'order_id','PostalCode':'zip'})
+googframe = googframe.rename(columns={'FirstName':'First Name','LastName':'Last Name','PostalCode':'Zip','EvePhone':'Phone','Email':'Email'})
+
+googframe['Country'] = 'US'
+
+fbaud = fbframe2[['fn','ln','st','country','email','phone']]
+goog2 = googframe[['First Name','Last Name','Email','Zip','Country','Phone']]
+
+fbframe2.to_csv('fbconversions.csv', index=False)
+#goog2.to_csv('googaud.csv', index=False)
+fbaud.to_csv('fbaud.csv', index=False)
+
+goog2 = goog2[['First Name','Last Name','Email','Zip','Country','Phone2']]
+goog2 = goog2.rename(columns={'Phone2':'Phone'})
+
+goog2.to_csv('googaud.csv', index=False)
